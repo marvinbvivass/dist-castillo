84da404507c0fc939b61bfdb937de0d5e25a3770
marvinbvivass <marvinbvivass@gmail.com>
2025-09-08 15:03:54 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,43 @@
-const CACHE_NAME = 'ventas-cache-v1';
-
-// Lista de archivos que queremos cachear
-const urlsToCache = [
-    '/',
-    'index.html',
-    'inventario.js',
-    'ventas.js',
-    'https://cdn.tailwindcss.com',
-    'https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js',
-    'https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js',
-    'https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js'
+const CACHE_NAME = 'ventas-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './inventario.js',
+  './ventas.js',
+  'https://cdn.tailwindcss.com',
+  'https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js',
+  'https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js'
 ];
 
-// Instalar el service worker y cachear los archivos
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(FILES_TO_CACHE);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
-// Servir los archivos desde el caché cuando sea posible
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Si el recurso está en el caché, lo devuelve
-                if (response) {
-                    return response;
-                }
-                // Si no, lo busca en la red
-                return fetch(event.request);
-            })
-    );
-});
-
-// Limpiar cachés antiguos
 self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
 });
 
