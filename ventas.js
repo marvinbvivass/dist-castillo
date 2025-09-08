c1458e14f5b08ee9021baaa7bf23a5acef004f93
marvinbvivass <marvinbvivass@gmail.com>
2025-09-08 14:30:41 -0400

    Update ventas.js

--- ventas.js
+++ ventas.js
@@ -1,1 +1,8 @@
-
+export function showVentasView(mainContent) {
+    mainContent.innerHTML = `
+        <div class="flex flex-col items-center gap-4">
+            <h2 class="text-2xl font-semibold text-gray-700 mb-2">Sección de Ventas</h2>
+            <p class="text-gray-500">La funcionalidad de ventas estará disponible aquí.</p>
+        </div>
+    `;
+}
